51a1aec2d0ab873999d2794a14e1ef1c4021f266
zafarxayrullayev <xayrullayevzafar49@gmail.com>
2022-12-03 16:07:07 +0500

    solve 14th issue

--- min-max-issues/Program.cs
+++ min-max-issues/Program.cs
@@ -77,35 +77,52 @@ namespace min_max_issues
             //    Console.WriteLine($"max element 1 - keladi : {maxId}");
             //}
 
-            int limit, num, min, max, minId = 1, maxId = 1;
+            //int limit, num, min, max, minId = 1, maxId = 1;
+            //Console.Write("Limit : ");
+            //limit = Convert.ToInt32(Console.ReadLine());
+            //num = Convert.ToInt32(Console.ReadLine());
+            //min = num;
+            //max = num;
+            //for (int i = 2; i <= limit; i++)
+            //{
+            //    num = Convert.ToInt32(Console.ReadLine());
+            //    if (min >= num)
+            //    {
+            //        min = num;
+            //        minId = i;
+            //    }
+            //    if (max <= num)
+            //    {
+            //        max = num;
+            //        maxId = i;
+            //    }
+            //}
+
+            //if (maxId < minId)
+            //{
+            //    Console.WriteLine($"min element oxirgi keladi : {minId}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"max element oxirgi keladi : {maxId}");
+            //}
+
+            int b, limit, num, min = 0, idMax = 0;
             Console.Write("Limit : ");
             limit = Convert.ToInt32(Console.ReadLine());
-            num = Convert.ToInt32(Console.ReadLine());
-            min = num;
-            max = num;
-            for (int i = 2; i <= limit; i++)
+            Console.Write("b = ");
+            b = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= limit; i++)
             {
                 num = Convert.ToInt32(Console.ReadLine());
-                if (min >= num)
+                if((num > b) && ((min > num) || (idMax == 0)))
                 {
                     min = num;
-                    minId = i;
-                }
-                if (max <= num)
-                {
-                    max = num;
-                    maxId = i;
+                    idMax= i;
                 }
             }
 
-            if (maxId < minId)
-            {
-                Console.WriteLine($"min element oxirgi keladi : {minId}");
-            }
-            else
-            {
-                Console.WriteLine($"max element oxirgi keladi : {maxId}");
-            }
+            Console.WriteLine($"1 - katta element indeksi : {idMax}");
         }
     }
 }
