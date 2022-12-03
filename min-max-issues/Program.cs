fe177ff1d9d53c0bf42a254b9662c5ad7da078ad
zafarxayrullayev <xayrullayevzafar49@gmail.com>
2022-12-03 15:09:33 +0500

    solve 1st issue in qudrat abdurahimov's book

---
+++ min-max-issues/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace min_max_issues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, limit, max, min;
+            Console.Write("Limit : ");
+            limit = Convert.ToInt32(Console.ReadLine());
+            num = Convert.ToInt32(Console.ReadLine());
+            max = num;
+            min = num;
+            for (int i = 1; i < limit; i++)
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+                if (min > num)
+                {
+                    min = num;
+                }
+                if (max < num)
+                {
+                    max = num;
+                }
+            }
+
+            Console.WriteLine($"max : {max},    min : {min}");
+        }
+    }
+}
